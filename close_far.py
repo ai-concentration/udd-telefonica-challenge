a6be738bb4629aca418b0a852134c3d0d1d8ede1
Carlos Hurtado <52082645+carlosahs@users.noreply.github.com>
2022-10-21 00:00:08 -0500

    Use smaller dataset and fix call to function

--- close_far.py
+++ close_far.py
@@ -14,7 +14,8 @@ dtype = {
     'lon': 'string',  # Handle as string to avoid floating precision errors
 }
 
-dataset = pd.read_csv(DATA_DIR / 'data.csv', dtype=dtype)
+# Load antenna geolocation dataset due to smaller size
+dataset = pd.read_csv(DATA_DIR / 'antenna_geolocation.csv', dtype=dtype)
 
 max_distance = 0
 min_distance = 10000
@@ -25,7 +26,10 @@ dic = {}
 for i in range(len(dataset['bts_id'])):
     distances = []
     for j in range(len(dataset['bts_id'])):
-        distance = DistanceKm(dataset['lat'][i],dataset['lon'][i],dataset['lat'][j],dataset['lon'][j])
+        distance = distance_km(
+            (np.radians(float(dataset['lat'][i])), np.radians(float(dataset['lon'][i]))),
+            (np.radians(float(dataset['lat'][j])), np.radians(float(dataset['lon'][j])))
+        )
         #distances.append(distance)
         if distance > max_distance:
             far = [dataset['lat'][j],dataset['lon'][j]]
