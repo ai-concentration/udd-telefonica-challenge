c3dbb3fca8fd52c06947b7e71daaa905d755afa0
Carlos Hurtado <52082645+carlosahs@users.noreply.github.com>
2022-11-11 12:39:00 -0600

    Use pandas apply and remove JSON file

--- Data_preparation/routes.py
+++ Data_preparation/routes.py
@@ -8,6 +8,7 @@ DTYPE = {
 }
 
 DATA_DIR = Path("data")  # Define directory for datasets
+JSON_DIR = Path("json")  # Define directory for JSONs
 
 # Loading the dataset
 print("Loading dataset...")
@@ -24,74 +25,79 @@ import json
 from collections import defaultdict
 from datetime import datetime  # So we can deal with timestamps strings
 
-# Each phone id will have a route (a list), which is composed of mobile connections
-phone_id_route = defaultdict(list)
-
-# Map bts_ids to coordinates
-bts_latlon = {}
-
-# Retrieve info for each mobile connection associated with a phone id
-print("Retrieving mobile connections...")
-
-for i in dataset.index:
-    # Get values by index
-    phone_id = dataset["PHONE_ID"][i]
-    timestamp = dataset["timestamp"][i]
-
-    lat = dataset["lat"][i]
-    lon = dataset["lon"][i]
-
-    bts_id = dataset["bts_id"][i]
-
-    # Only consider the lat and lon of the first bts_id instance
-    if bts_id in bts_latlon:
-        lat = bts_latlon[bts_id][0]
-        lon = bts_latlon[bts_id][1]
-    else:
-        bts_latlon[bts_id] = (lat, lon)
-
-    phone_id_route[phone_id].append((timestamp, lat, lon))
-
-print("Done!")
-
 # Specify time format by which timestamp strings will be parsed
 TIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%f%z"
 
-# Sort mobile connections by timestamp
-print("Sorting mobile connections by timestamp...")
+# Get first timestamp in dataset and convert it to datetime object
+FIRST_TIMESTAMP = datetime.strptime(dataset["timestamp"][0], TIME_FORMAT)
 
-for key in phone_id_route.keys():
-    phone_id_route[key].sort(
-        # Parse timestamp strings to datetime objects
-        key=lambda mobile_connection: datetime.strptime(
-            mobile_connection[0],
-            TIME_FORMAT
-        )
-    )
-    
-print("Done!")
 
-# Store dict in JSON file
-import os
+def get_time_dummy(timestamp):
+    """
+    Convert each timestamp from dataset to time dummy
+    """
+    timestamp = datetime.strptime(timestamp, TIME_FORMAT)
 
-# Create directory to store JSON file
-JSON_DIR = Path("json/routes")
+    seconds_delta = timestamp - FIRST_TIMESTAMP
 
-try:
-    os.mkdir(JSON_DIR)
-except FileExistsError:
-    print("Directory already exists")
-except:
-    # Abort program in case other exceptions occur
-    raise Exception("Directory could not be created")
+    return int(seconds_delta.total_seconds())  # Time dummies are computed per second
 
-# Store routes JSON file
-print("Storing JSON files...")
 
-with open(JSON_DIR / "routes.json", "w") as f:
-    json.dump(phone_id_route, f)
+# Update timestamp column with time dummy values
+print("Converting timestamp values to time dummies...")
+
+dataset["timestamp"] = dataset["timestamp"].apply(get_time_dummy)  # apply method performs in parallel
 
 print("Done!")
 
-# Print number of phone ids in dataset
-print("# of phone IDs:", len(phone_id_route))
+bts_latlon_mapping = None
+
+print("Loading bts/lat lon mapping...")
+
+with open(JSON_DIR / "bts_latlon_mapping.json", "r") as f:
+    bts_latlon_mapping = json.load(f)
+
+print("Done!")
+
+
+def clean_lat_lon(bts_id):
+    lat = bts_latlon_mapping[bts_id].split(",")[0]
+    lon = bts_latlon_mapping[bts_id].split(",")[1]
+
+    return lat, lon
+
+
+# Update lat and lon columns
+print("Updating lat and lon columns...")
+
+dataset["lat"], dataset["lon"] = zip(*dataset["bts_id"].apply(clean_lat_lon))
+
+print("Done!")
+
+# Get min value from timestamp time dummies
+MIN_TIME_DUMMY = dataset["timestamp"].min()
+
+# Update time dummies to go from 0 to max + min instead of from min to max
+dataset["timestamp"] -= MIN_TIME_DUMMY
+
+# Sort by timestamp and by phone ID
+print("Sorting dataset...")
+
+dataset.sort_values(
+    by=["PHONE_ID", "timestamp"],
+    inplace=True,
+    ascending=True
+)
+
+print("Done!")
+
+
+# Store updated dataset
+print("Saving updated dataset...")
+
+dataset.to_csv(
+    path_or_buf=DATA_DIR / "updated_data.csv",
+    index=False
+)
+
+print("Done!")
