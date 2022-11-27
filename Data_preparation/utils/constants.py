8066fc90eb7ee6b51f013ceee687f4d071905a64
Carlos Hurtado <52082645+carlosahs@users.noreply.github.com>
2022-11-26 20:52:19 -0600

    Update constants

--- Data_preparation/utils/constants.py
+++ Data_preparation/utils/constants.py
@@ -7,14 +7,27 @@ from pathlib import Path
 DATA_DIR = Path("data")
 JSON_DIR = Path("json")
 
-# Dataframe related constants
-DATA_DTYPE = {
+# Dataframe related constants for data.csv
+DTYPE_DATA_DF = {
     "PHONE_ID": "string",
     "timestamp": "datetime64[ns, UTC]",
     "bts_id": "string",
     "lat": np.float64,
     "lon": np.float64
 }
+
+# Specify types of columns for routes_ready.csv
+DTYPE_UPDATED_DF = {
+    "PHONE_ID": "string",
+    "bts_id": "string",
+    "antenna id": np.int64,
+    "timestamp": np.int64,
+    "lat": np.float64,
+    "lon": np.float64
+}
+
+# Constant to specify chunk size when reading dataframes by chunk
+CHUNKSIZE = 1e5  # For datasets over a 1M entries
 
 # Data transformation related
 DECIMALS = 4
