8cc2fe939abb7cb33efeb42eb91aff4b546b103c
Carlos Hurtado <52082645+carlosahs@users.noreply.github.com>
2022-11-26 20:58:17 -0600

    Use antenna ID from updated dataset instead of bts ID

--- Data_preparation/comunas_geopandas.py
+++ Data_preparation/comunas_geopandas.py
@@ -7,27 +7,18 @@ from shapely.geometry import Point, Polygon
 from collections import defaultdict
 from pathlib import Path
 
-from utils.storers import JSONStorer, CSVStorer
+from utils.constants import DTYPE_UPDATED_DF
+from utils.storers import JSONStorer, CSVStorer, DATA_DIR
 
 
 def get_comunas_bts_dict(antenna_df, comunas_geodata):
     # Create list of comunas to insert later as a column in antenna_df
     comunas = []
 
-    # bts_id to lat and lon mapping
-    bts_latlon_mapping = {}
-
     for i in antenna_df.index:
         # Get lat and lon strings
         lat = antenna_df["lat"][i]
         lon = antenna_df["lon"][i]
-
-        # Get bts_id by index
-        bts_id = antenna_df["bts_id"][i]
-
-        # Convert lat and lon to string to floats
-        lat = float(lat)
-        lon = float(lon)
 
         # Create geopandas Point to determine comuna
         antenna_loc = Point(lon, lat)
@@ -41,44 +32,27 @@ def get_comunas_bts_dict(antenna_df, comunas_geodata):
                 comunas.append(comuna)
                 break
 
-        bts_latlon_mapping[bts_id] = f"{lat},{lon}"
-
     antenna_df["comuna"] = comunas
 
     # Sort by bts_id in descending order
-    antenna_df.sort_values(by=["comuna", "bts_id"], inplace=True, ascending=True)
+    antenna_df.sort_values(by=["comuna", "antenna id"], inplace=True, ascending=True)
 
     # Store antenna geolocation dataframe
     CSVStorer("antenna_geolocation", antenna_df).store()
 
-    # Store antenna geolocation dataframe
-    JSONStorer("bts_latlon_mapping", bts_latlon_mapping).store()
-
 
 if __name__ == '__main__':
-    # Define dataset column dtypes
-    DATA_DTYPE = {
-        "PHONE_ID": "string",
-        "timestamp": "string",
-        "bts_id": "string",
-        "lat": "string",  # Handling lat as string avoids floating precision errors
-        "lon": "string"  # Handling lon as string avoids floating precision errors
-    }
-    
-    #load dataset
-    DATA_DIR = Path("data")
-    
     print("Loading dataset..")
 
     dataset = pd.read_csv(
-        DATA_DIR / 'data.csv',
-        dtype=DATA_DTYPE
+        DATA_DIR / 'updated_data.csv',
+        dtype=DTYPE_UPDATED_DF
     )  
     
     print('Done!')
 
     # Drop duplicate bts_ids
-    dataset.drop_duplicates(subset=["bts_id"], inplace=True, keep="first")
+    dataset.drop_duplicates(subset=["antenna id"], inplace=True, keep="first")
     
     # Drop phone and timestamp data to work only with antenna data
     dataset.drop(["PHONE_ID", "timestamp"], axis=1, inplace=True)
